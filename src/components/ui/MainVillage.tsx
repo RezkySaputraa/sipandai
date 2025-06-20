daef084f3c3311769833443d008fea5c0c72934b
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-06-20 20:40:29 +0800

    Finishing User AI

--- src/components/ui/MainVillage.tsx
+++ src/components/ui/MainVillage.tsx
@@ -2,26 +2,32 @@
 
 import { useState } from "react";
 import BudgetTable from "./BudgetTable";
+import Komentar from "./Komentar";
+import SummaryAi from "./SummaryAi";
 
 export default function MainVillage() {
   const [table, setTable] = useState(true);
 
   return (
-    <div className="w-9/12 bg-white rounded-xl p-4">
+    <div className="w-9/12 bg-white rounded-xl p-4 mb-4">
       <h1 className="font-bold text-xl text-[#16604B]">
         Total Anggaran & Realisasi Desa Bati-Bati
       </h1>
-      <p className=" text-gray-500">Terakhir diperbaharui : 20 Desember 2022</p>
-      <button className="bg-[#0093DD] w-1/12 text-white rounded-lg font-semibold p-1">
-        Kembali
-      </button>
-      <button className="bg-[#5DAB2C] w-1/12 text-white rounded-lg font-semibold p-1">
-        Unduh
-      </button>
-      <button className="bg-[#E20303] w-1/12 text-white rounded-lg font-semibold p-1">
-        Lapor
-      </button>
-      <div>
+      <p className=" text-gray-500 py-4">
+        Terakhir diperbaharui : 20 Desember 2022
+      </p>
+      <div className="flex gap-2">
+        <button className="bg-[#0093DD] w-1/12 text-white rounded-lg font-semibold p-1">
+          Kembali
+        </button>
+        <button className="bg-[#5DAB2C] w-1/12 text-white rounded-lg font-semibold p-1">
+          Unduh
+        </button>
+        <button className="bg-[#E20303] w-1/12 text-white rounded-lg font-semibold p-1">
+          Lapor
+        </button>
+      </div>
+      <div className="flex gap-2 py-4">
         <select
           name="year"
           id="year"
@@ -52,12 +58,28 @@ export default function MainVillage() {
         </select>
       </div>
 
-      <div className="flex gap-3">
-        <h1 onClick={() => setTable(true)}>Data Tabel</h1>
-        <h1 onClick={() => setTable(false)}>AI Summary</h1>
+      <div className="flex gap-3 mb-4">
+        <h1
+          onClick={() => setTable(true)}
+          className={`${
+            table && "text-[#0093DD]"
+          } font-semibold cursor-pointer`}
+        >
+          Data Tabel
+        </h1>
+        <h1
+          onClick={() => setTable(false)}
+          className={`${
+            !table && "text-[#0093DD]"
+          } font-semibold cursor-pointer`}
+        >
+          AI Summary
+        </h1>
       </div>
 
-      {table && <BudgetTable></BudgetTable>}
+      {table ? <BudgetTable></BudgetTable> : <SummaryAi></SummaryAi>}
+
+      <Komentar></Komentar>
     </div>
   );
 }
